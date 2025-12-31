69c9e56ee53f5289ef987be56fdb64af4c514719
kelyonnnn17 <kalyan15122005@gmail.com>
2026-01-01 02:53:19 +0530

    Added Android support and fixed status bar layout

---
+++ src/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vector.app',
+  appName: 'vector',
+  webDir: 'dist'
+};
+
+export default config;
